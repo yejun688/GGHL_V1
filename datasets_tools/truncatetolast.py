d531cfadcded61bdc4473c813d977bce82152fee
yejun <1548622388@qq.com>
2022-11-02 23:00:47 +0800

    feat：提交初始修改代码。

---
+++ datasets_tools/truncatetolast.py
@@ -0,0 +1,10 @@
+import os
+
+# path = '../../datasets/slam_parkingslot/new_data/dota_ann/'
+label_path = '/data0/datasets/slam_parkingslot/new_data/newdata/'
+
+files = os.listdir(label_path)
+print(len(files))
+print(files)
+
+
